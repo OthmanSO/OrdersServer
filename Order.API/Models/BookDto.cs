f401425a78e46c6a5938b9e64d07040f85154f9c
OthmanSO <Othman.subhi.123@gmail.com>
2022-07-24 03:13:50 +0300

    Add Refit functionality
    -Add controller
    -Add BookDto
    -Add IBookDataAccessRepo to interact with remoteserver

---
+++ Order.API/Models/BookDto.cs
@@ -0,0 +1,10 @@
+
+namespace Order.API.Models
+{
+    public class BookDto
+    {
+        public String title { get; set; }
+        public int quantity { get; set; }
+        public int price { get; set; }
+    }
+}
---
+++ Order.API/Repositories/IBookSDataAccessRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Order.API.Models;
+using Refit;
+namespace Order.API.Repositories
+{
+    public interface IBookSDataAccessRepository
+    {
+        [Get("books/{id}")]
+        Task<BookDto> GetBook(int id);
+        [Put("books/{id}")]
+        Task<BookDto> PutBook([Body] BookDto book);
+    }
+}
